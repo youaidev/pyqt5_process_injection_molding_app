ab572440467b7afc73932b7d96b9268da11e8031
Youcef AIDOUN <108759297+youaidev@users.noreply.github.com>
2023-04-21 09:53:00 -0700

    testing run of app

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-import PyQt5
+""" Mudules """
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+import sys
+
+
+# app
+app = QApplication(sys.argv)
+
+# window
+window = QWidget()
+window.setWindowTitle("Process Notes")
+window.show()
+
+# exit app
+app.exec()
